82d024539876eab4ce574ae09e25967892a39f85
Edbless <giftyhemans8@gmail.com>
2025-09-23 10:51:10 +0000

    Components added

---
+++ src/app/Core/assigned-task/assigned-task.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assigned-task',
+  imports: [],
+  templateUrl: './assigned-task.html',
+  styleUrl: './assigned-task.css'
+})
+export class AssignedTask {
+
+}
---
+++ src/app/forms/add-task-users/add-task-users.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-users',
+  imports: [],
+  templateUrl: './add-task-users.html',
+  styleUrl: './add-task-users.css'
+})
+export class AddTaskUsers {
+
+}
