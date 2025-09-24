1d2e6b6b2c33bb8d143207ae5263b0d193e77751
philip_jalloh <jallohphilip1999@gmail.com>
2025-09-24 09:30:52 +0000

    Add-task component

--- src/app/forms/add-task/add-task.ts
+++ src/app/forms/add-task/add-task.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../Core/model/task.model';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.html',
   styleUrl: './add-task.css'
 })
 export class AddTask {
+title = '';
+  description = '';
 
+  constructor(private taskService: TaskService) {}
+
+  addTask() {
+    if (this.title.trim() && this.description.trim()) {
+      const newTask: Task = {
+        id: Date.now(), // simple unique id
+        title: this.title,
+        description: this.description,
+        assignedTo: null,
+        completed: false
+      };
+
+      this.taskService.addTask(newTask);
+
+      // reset form
+      this.title = '';
+      this.description = '';
+      alert('Task added successfully!');
+    }
+  }
 }
